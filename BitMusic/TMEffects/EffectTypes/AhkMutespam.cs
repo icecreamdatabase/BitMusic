159bbc9f86aa38665d7e59e47facf202406daf18
Karl <git@karlneugebauer.com>
2023-08-03 20:23:28 +0200

    added tooltip and mute spam

---
+++ BitMusic/TMEffects/EffectTypes/AhkMutespam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BitMusic.TMEffects.EffectHelper;
+
+namespace BitMusic.TMEffects.EffectTypes;
+
+public class AhkMutespam : EffectBase
+{
+    public int ActiveTimeMs { get; }
+    public int MuteDurationMs { get; }
+    public int UnmuteDurationMs { get; }
+
+    public AhkMutespam(int weight, int activeTimeMs, int muteDurationMs = 150, int unmuteDurationMs = 50) :
+        base(weight)
+    {
+        ActiveTimeMs = activeTimeMs;
+        MuteDurationMs = muteDurationMs;
+        UnmuteDurationMs = unmuteDurationMs;
+    }
+    
+    public override string GetConsoleOutput()
+    {
+        return $"🏎 Spamming mute for {Math.Round(ActiveTimeMs / 1000f, 1)} s";
+    }
+
+    public override void Execute(string processName)
+    {
+        int repeatCount = (int)(ActiveTimeMs / (float)(MuteDurationMs + UnmuteDurationMs));
+
+        string code = $$"""
+                        #IfWinActive ahk_exe {{processName}}
+                        Loop, {{repeatCount}} {
+                            SoundSet, +1,, Mute
+                            Sleep, {{MuteDurationMs}}
+                            SoundSet, +1,, Mute
+                            Sleep, {{UnmuteDurationMs}}
+                        }
+                        """;
+
+        AhkHelper.ExecuteAhkScript(code);
+    }
+}
---
+++ BitMusic/TMEffects/EffectTypes/AhkTooltip.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BitMusic.TMEffects.EffectHelper;
+
+namespace BitMusic.TMEffects.EffectTypes;
+
+public class AhkTooltip : EffectBase
+{
+    private Random _offsetRandom = new();
+
+    public string Text { get; }
+    public int TimeBetweenJumpsMs { get; }
+    public int Jumps { get; }
+
+    public AhkTooltip(int weight, string text, int timeBetweenJumpsMs, int jumps) : base(weight)
+    {
+        Text = text;
+        TimeBetweenJumpsMs = timeBetweenJumpsMs;
+        Jumps = jumps;
+    }
+
+    public override string GetConsoleOutput()
+    {
+        return "📓 Showing tooltips";
+    }
+
+    public override void Execute(string processName)
+    {
+        string code = $$"""
+                        #IfWinActive ahk_exe {{processName}}"
+                        Loop, {{Jumps}} {
+                            Random, xOffset, -300, 300
+                            Random, yOffset, -300, 300
+                            ToolTip, `n`n        {{Text}}        `n`n, A_ScreenWidth //2 + xOffset, A_ScreenHeight //2 + yOffset
+                            Sleep, {{TimeBetweenJumpsMs}}
+                        }
+                        ToolTip
+                        """;
+
+        AhkHelper.ExecuteAhkScript(code);
+    }
+}
